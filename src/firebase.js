d08333cc2bf5aa003fcb25a95f7719d92baaf28a
juncheng <wongjun_cheng@outlook.com>
2021-06-30 22:24:05 +0800

    delete dotenv

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
+import dotenv from "dotenv";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB2kwx78UNaClKoaXeH4IRYTZsR2tBS8Zc",
-    authDomain: "clone-5bdd0.firebaseapp.com",
-    projectId: "clone-5bdd0",
-    storageBucket: "clone-5bdd0.appspot.com",
-    messagingSenderId: "272402658528",
-    appId: "1:272402658528:web:5c0e2418776ff6e2a68c85",
-    measurementId: "G-FV1CZT2DD8"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
